ab568d2ddb42aa1e42914759dd104771486d35fe
manuusc <septembermanu@gmail.com>
2017-04-23 22:20:19 -0700

    fixing action_mailer url in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "salty-inlet-40510.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "siliconvalley-apartments.herokuapp.com" }
 
 end
