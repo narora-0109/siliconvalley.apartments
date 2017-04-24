1d9d71ddd980db0f938b6751c2f6eb9c79ea89c4
manuusc <septembermanu@gmail.com>
2017-04-23 22:14:31 -0700

    fixing migration issue

--- db/migrate/20170412080243_add_fields_to_listings.rb
+++ db/migrate/20170412080243_add_fields_to_listings.rb
@@ -4,5 +4,7 @@ class AddFieldsToListings < ActiveRecord::Migration[5.0]
     add_column :listings, :country, :string
     add_column :listings, :state, :string
     add_column :listings, :property_desc, :text
+    add_column :listings, :latitude, :string
+    add_column :listings, :longitude, :string
   end
 end
