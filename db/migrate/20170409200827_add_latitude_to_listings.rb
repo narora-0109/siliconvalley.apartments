317bcd6851bee1567603f6051d7a34b63e14c34f
manuusc <septembermanu@gmail.com>
2017-04-23 22:10:59 -0700

    fixing migration issue

--- db/migrate/20170409200827_add_latitude_to_listings.rb
+++ db/migrate/20170409200827_add_latitude_to_listings.rb
@@ -1,6 +1,8 @@
 class AddLatitudeToListings < ActiveRecord::Migration
   def change_table
-    add_column :listings, :latitude, :decimal, :precision => 10, :scale => 6
-    add_column :listings, :longitude, :decimal, :precision =>10, :scale => 6
+    add_column :listings, :latitude, :string
+    add_column :listings, :longitude, :string
   end
 end
+
+
--- db/migrate/20170418060306_change_type_latllong.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTypeLatllong < ActiveRecord::Migration[5.0]
-  def change
-    change_column :listings, :latitude, :string
-    change_column :listings, :longitude, :string
-  end
-end
